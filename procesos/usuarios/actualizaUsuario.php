e0aec37737f7ad22a451c5c46a13dde54b55327b
LM-8941 <66273409+LM-8941@users.noreply.github.com>
2025-06-16 21:00:59 -0600

    Agregacion de roles

--- procesos/usuarios/actualizaUsuario.php
+++ procesos/usuarios/actualizaUsuario.php
@@ -4,15 +4,14 @@
 	require_once "../../clases/Usuarios.php";
 
 	$obj= new usuarios;
-
 	$datos=array(
 			$_POST['idUsuario'],  
 		    $_POST['nombreU'] , 
 		    $_POST['apellidoU'],  
-		    $_POST['usuarioU']
-				);  
+		    $_POST['usuarioU'],
+		    $_POST['rolU']
+				);
 	echo $obj->actualizaUsuario($datos);
 
-	
 
  ?>
